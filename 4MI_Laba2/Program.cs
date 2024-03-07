14083bfbb812e2e360559d094ae5ffc69166ed05
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2024-03-07 15:38:56 +0200

    Initial commit

---
+++ 4MI_Laba2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4MI_Laba2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double[] testVectorA = new double[] { 1, 2, 3 };
+            double[,] testMatrixX = new double[,]
+            {
+                {4, 4, 4},
+                {5, 5, 5},
+                {6, 6, 6}
+            };
+            double[] testVectorB = new double[] { 7, 8, 9 };
+            
+            SLE sle = new SLE(testVectorA, testMatrixX, testVectorB);
+            Console.WriteLine(sle);
+        }
+    }
+}
---
+++ 4MI_Laba2/SLE.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4MI_Laba2
+{
+    public class SLE
+    {
+        public double[]  vector_X;
+        public double[,] matrix_A;
+        public double[]  vector_B;
+
+        public SLE(double[] x, double[,] a, double[] b)
+        {
+            vector_X = x;
+            matrix_A = a;
+            vector_B = b;
+        }
+
+        public Equation GetRowEq(int idxRow)
+        {
+            double[] coeffs = new double[matrix_A.GetLength(1)];
+            for (int i = 0; i < matrix_A.GetLength(0); i++)
+            {
+                
+            }
+            return new Equation(coeffs, vector_X, vector_B[idxRow]);
+        }
+        
+        public override string ToString()
+        {
+            List<string> res = new List<string>();
+            for (int i = 0; i < matrix_A.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix_A.GetLength(1); j++)
+                {
+                    res.Add($"{matrix_A[i, j]}*{vector_X[i]} ");
+                }
+                res.Add($"= {vector_B[i]}\n");
+            }
+
+            return string.Join("", res);
+        }
+    }
+
+    public class Equation
+    {
+        public double[] coefficients;
+        public double[] variables;
+        public double constTerm;
+
+        public Equation(double[] coef, double[] vars, double c)
+        {
+            coefficients = coef;
+            variables = vars;
+            constTerm = c;
+        }
+        
+    }
+}
